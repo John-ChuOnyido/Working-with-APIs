b2168821d42e2a4af7dbcc05d52eb71eab99df95
John-ChuOnyido <onyidojohn@gmail.com>
2022-07-03 11:56:43 +0100

    modified

--- I4G0149033YF/links/views.py
+++ I4G0149033YF/links/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Link
+from .models import LinkSerializer
+from django.views.generic.list import ListAPIView
+from django.views.generic.edit import CreateAPIView
+from django.views.generic.detail import DetailAPIView
+from django.views.generic.edit import UpdateAPIView
+from django.views.generic.edit import DeleteAPIView
+
+class PostListApi(ListAPIView):
+    queryset = Link.object.filter(active=True) 
+    serializer_class = LinkSerializer
+    
+class PostCreateApi(CreateAPIView):
+    queryset = Link.object.filter(active=True) 
+    serializer_class = LinkSerializer
+    
+  
+class PostDetailApi(DetailAPIView):
+    queryset = Link.object.filter(active=True) 
+    serializer_class = LinkSerializer
+    
+    
+class PostUpdateApi(UpdateAPIView):
+   queryset = Link.object.filter(active=True)
+   serializer_class = LinkSerializer
+    
+class PostDeleteApi(DeleteAPIView):
+    queryset = Link.object.filter(active=True) 
+    serializer_class = LinkSerializer
+    
